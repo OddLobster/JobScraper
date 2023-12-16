d5566d12d409cddc813cd402916094cb2af96317
Raphael <raphael.fruehwirth@student.tugraz.at>
2023-12-16 17:34:04 +0100

    rendered certain pages in selenium

--- page_crawl.py
+++ page_crawl.py
@@ -6,36 +6,74 @@ import threading
 import time
 import random
 import hashlib
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 database = JobDB()
 
 TOTAL_NUM_URLS = JobDB().get_num_documents()
 BATCH_SIZE = 250
 
+selectors = {
+    "selenium-description": ["body > div.l-master > div.l-master__content > div > div > div > div.m-jobContent > div.m-jobContent__jobDetail > div.m-jobContent__jobText.m-jobContent__jobText--standalone", "body > div > div.l-master__content > div > div > div > div.m-jobContent > div.m-jobContent__jobDetail > div.m-jobContent__jobText.m-jobContent__jobText--standalone", "body > div.l-master > div.l-master__content > div > div > div > div.m-jobContent > div.m-jobContent__jobDetail > div.m-jobContent__jobText.m-jobContent__jobText--standalone", "body > div.l-master > div.l-master__content > div > div > div > div.m-jobContent > div.m-jobContent__jobDetail > div.m-jobContent__jobText.m-jobContent__jobText--standalone"],
+    "regular-description": ["body > div > div.l-master__content > div > div > div > div.m-jobContent > div.m-jobContent__jobDetail > div.m-jobContent__jobText.m-jobContent__jobText--standalone", "#k2-template > div > div.content"]
+}
+
+def get_description(selectors, parser):
+    def try_get_description(selector):
+        try:
+            description = parser.css(selector)[0].text()
+        except:
+            return ""
+        return description
+    
+    for selector in selectors:
+        full_description = try_get_description(selector)
+        if full_description != "":
+            return full_description
+    print("No description found")
+    return "no description"
+
+
 def crawl_pages(skip_to, NUM_URLS=100):
     job_db = JobDB()
     urls = job_db.get_items_to_update(skip=skip_to, num_urls=NUM_URLS)
+
+    options = webdriver.ChromeOptions()
+    # options.add_argument("start-maximized")
+    options.add_argument("--headless")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--ignore-certificate-errors')
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
     with httpx.Client(base_url="https://www.karriere.at") as client:
         for data in urls:
             response = client.get(data[1])
-            if response.status_code != 200:
-                print("Something wrong with request")
+            if response.status_code == 301 or response.status_code == 404:
+                print(f"Job inactive {data[1]}")
+                job_db.update_item(is_active=False, id=data[0], full_description=data[2], updated_at=datetime.datetime.now())
                 continue
+
+            elif response.status_code == 500:
+                print(f"load with selenium {data[1]}")
+
+                driver.get(data[1])
+                html = driver.page_source
+                parser = HTMLParser(html)
+                full_description = get_description(selectors["selenium-description"], parser)
+                job_db.update_item(is_active=True, id=data[0], full_description=full_description, updated_at=datetime.datetime.now())
+                time.sleep(random.uniform(0.5, 1.5)) # trunk-ignore(bandit/B311)
+                continue
+
+            elif response.status_code != 200:
+                print(f"Something wrong with request {response.status_code} {data[1]}")
+                continue
+
             html = response.text
             parser = HTMLParser(html)
-            try:
-                full_description = parser.css("body > div > div.l-master__content > div > div > div > div.m-jobContent > div.m-jobContent__jobDetail > div.m-jobContent__jobText.m-jobContent__jobText--standalone")[0].text()
-            except Exception:
-                try:
-                    full_description = parser.css("#k2-template > div > div.content")[0].text()
-                except Exception as e:
-                    print(f"{data[1]} needs special treatment or doesnt exist \n{e}")
-                    # with open("test.html", "w+") as file:
-                    #     file.write(html)
-                    job_db.update_item(is_active=False, id=data[0], full_description=data[2], updated_at=datetime.datetime.now())
-                    continue
-            finally:
-                full_description = "no description"
+            full_description = get_description(selectors["regular-description"], parser)
 
             # encoder = hashlib.sha256()
             # encoder.update(full_description.encode("utf-8"))
@@ -49,6 +87,7 @@ def crawl_pages(skip_to, NUM_URLS=100):
 
             job_db.update_item(is_active=True, id=data[0], full_description=full_description, updated_at=datetime.datetime.now())
             time.sleep(random.uniform(0.5, 1.5)) # trunk-ignore(bandit/B311)
+
         print(f"Thread {skip_to//BATCH_SIZE} done")
             
 
